19d76a12c7b479ff5a902a1b4aa224b999d440b1
Zero-miss-0 <miss.zero.1.000@gmail.com>
2026-07-03 22:58:24 +0330

    Add files via upload

---
+++ task_manager/models/__init__.py
@@ -0,0 +1,5 @@
+from models.project import Project
+from models.task import Task
+from models.personal_task import PersonalTask
+from models.team_task import TeamTask
+from models.urgent_task import UrgentTask
---
+++ task_manager/services/__init__.py
@@ -0,0 +1,2 @@
+from services.manager import TaskManager
+from services.reports import completed_tasks, pending_tasks, total_tasks, project_progress, sorted_by_priority
